43c71e898bc410bcf11a9ba05048698bd706d899
Shamgar Staines <64740760+shamgar-kommerla@users.noreply.github.com>
2021-04-18 09:59:35 +0530

    Navbar Component, Dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {Component} from 'react';
+import Navbar from './Components/layout/navbar';
+import Dashboard from './Components/dashboard/Dashboard';
 
 class App extends Component{
   render(){
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>ShamgarPlan</h1>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </div>
       </BrowserRouter>
-     
     );
   }
 }
