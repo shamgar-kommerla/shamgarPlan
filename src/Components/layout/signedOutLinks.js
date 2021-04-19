ccda819a5669b44a182d4649c7e37143d0388daf
Shamgar Staines <64740760+shamgar-kommerla@users.noreply.github.com>
2021-04-19 17:28:31 +0530

    Additional CSS & Added Redux and Reducers

--- src/Components/layout/signedOutLinks.js
+++ src/Components/layout/signedOutLinks.js
@@ -5,10 +5,10 @@ const SignedOutLinks = () => {
     return(
         <ul className="right">
             <li>
-                <NavLink to="/">Sign up</NavLink>
+                <NavLink to="/signup">Sign up</NavLink>
             </li>
             <li>
-                <NavLink to="/">Log in</NavLink>
+                <NavLink to="/signin">Log in</NavLink>
             </li>
         </ul>
     );
